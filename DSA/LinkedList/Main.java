72b1daae8e7e160182a32fd347244efd3e67354a
acu8 <ewatanabe786@gmail.com>
2023-06-04 10:26:21 +0900

    basic method completed

--- DSA/LinkedList/Main.java
+++ DSA/LinkedList/Main.java
@@ -2,6 +2,11 @@ package LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.removeFirst();
+        // System.out.println(list.contains(40));
     }
 }
