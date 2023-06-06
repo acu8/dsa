8878ead828299c50d42576a942ff9a9b39690b93
acu8 <ewatanabe786@gmail.com>
2023-06-06 21:25:30 +0900

    LinkedList

--- DSA/LinkedList/Main.java
+++ DSA/LinkedList/Main.java
@@ -1,12 +1,19 @@
 package LinkedList;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.addLast(10);
+
+        list.addFirst(10);
         list.addLast(20);
         list.addLast(30);
-        list.removeFirst();
+        list.reverse();
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
+
         // System.out.println(list.contains(40));
     }
+
 }
